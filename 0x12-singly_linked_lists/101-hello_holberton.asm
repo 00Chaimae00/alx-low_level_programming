439e44ebb69cd1b18226d19d9566bf63afba2088
00Chaimae00 <chaimae.bellhamamia@outlook.fr>
2023-08-24 17:28:04 -0700

    hghtdh

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+section          .text
+	  extern    printf
+	global   main
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+section          .data
+        msg db 'Hello, Holberton', 0xa, 0     
